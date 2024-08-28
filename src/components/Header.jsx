4fc10efaa1aa5061f133d1c922c87266a9fe8cbe
Sedat Polat <sedatpolat355@gmail.com>
2024-08-28 17:39:37 +0300

    header content placed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,25 @@
+
+
 const Header = () => {
   return(
-    <header className="header">sidebar</header>
+    <header className="header">
+      <h1 className="logo-title">
+        <img src="logo.png" className="logo"/>
+        <a className="title">
+          <span className="first">Brand</span>
+          <span className="second">Colors</span>
+        </a>
+      </h1>
+
+      <p className="desc">
+      The biggest collection of official brand color codes around. Curated by <a>@brandcolors</a> and friends.
+      </p>
+
+      <div className="header-links">
+        <a className="suggest-link">Suggest a Brand</a>
+        <a className="about-link">About BrandColors</a>
+      </div>
+    </header>
   )
 }
 
